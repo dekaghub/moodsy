4961534ea5f72ed70b85172eb6671bce1af4fe91
Katherine Ray <ray.katherine.m@gmail.com>
2020-02-23 02:45:12 -0500

    start importing moodsy stuff

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask, request, redirect, jsonify
 import os
 from werkzeug.utils import secure_filename
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+clientID = '5e3b1a5db9854632bf9207c20eb44424'
+clientSecret = '0c093a2a8bb24aa8bee9b5ff36866cc5'
+
+client_creds = SpotifyClientCredentials(client_id=clientID, client_secret=clientSecret)
+sp = spotipy.Spotify(client_credentials_manager=client_creds)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
