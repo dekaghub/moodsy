e1a2274084bf98c72d2da050741cb24d99fd7e0f
Adam Myers <abmyers@bgsu.edu>
2020-02-23 07:23:49 -0500

    added basic mobile view

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, request, redirect, jsonify, render_template, session
 import os
 import io
 from werkzeug.utils import secure_filename
+from werkzeug.useragents import UserAgent
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from google.cloud import vision
 import analyze_image
+from flask_mobility import Mobility
+from flask_mobility.decorators import mobile_template
 
 
 # google creds
@@ -21,13 +24,23 @@ client_creds = SpotifyClientCredentials(
 sp = spotipy.Spotify(client_credentials_manager=client_creds)
 
 app = Flask(__name__)
+Mobility(app)
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.secret_key = "iLovmygorlfrein_wowSheCOOL"
 
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+@mobile_template('{mobile/}index.html')
+def index(template):
+    # mobile_devices = ['android', 'iphone', 'ipad', 'blackberry']
+    # user_agent = request.user_agent
+    # print(user_agent.platform, flush=True)
+    # if any(mobile_device in user_agent.platform for mobile_device in mobile_devices):
+    #     return render_template('mobile/index.html')
+    # else:
+    #     return render_template('index.html')
+
+    return render_template(template, title='home')
 
 
 @app.route("/sendfile", methods=["POST"])
