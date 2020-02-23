4976a0c1ceb9ce8c6f77fb3871fde592ef912b79
Katherine Ray <ray.katherine.m@gmail.com>
2020-02-22 20:38:26 -0500

    added file drop box and linked css

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from requests import post
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+    # fileobject = request.files["file2upload"]
+    # filename = secure_filename(fileobject.filename)
+    # save_path = "{}/{}".format(app.config["UPLOAD_FOLDER"], filename)
+    # fileobject.save(save_path)
+    # return "successful_upload"
 
 if __name__ == '__main__':
     app.run (debug=True)
