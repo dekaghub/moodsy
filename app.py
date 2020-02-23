d897e85860faa7253c0a2161bade502f933bb8e5
Katherine Ray <ray.katherine.m@gmail.com>
2020-02-23 04:02:23 -0500

    processes image properly. need to do something with output, but for now displays all as text

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
-from flask import Flask, request, redirect, jsonify, render_template
-import os, io
+from flask import Flask, request, redirect, jsonify, render_template, session
+import os
+import io
 from werkzeug.utils import secure_filename
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from google.cloud import vision
+import facesearch
 
-#google creds
+
+# google creds
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'service-account-token.json'
 
-#spotify creds
+# spotify creds
 clientID = '5e3b1a5db9854632bf9207c20eb44424'
 clientSecret = '0c093a2a8bb24aa8bee9b5ff36866cc5'
 
-client_creds = SpotifyClientCredentials(client_id=clientID, client_secret=clientSecret)
+
+client_creds = SpotifyClientCredentials(
+    client_id=clientID, client_secret=clientSecret)
 sp = spotipy.Spotify(client_credentials_manager=client_creds)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads'
-
+app.secret_key = "iLovmygorlfrein_wowSheCOOL"
 
 
 @app.route("/")
 def index():
     return redirect("/static/index.html")
+
 
 @app.route("/sendfile", methods=["POST"])
 def send_file():
@@ -30,10 +36,14 @@ def send_file():
     filename = secure_filename(fileob.filename)
     save_path = "{}/{}".format(app.config["UPLOAD_FOLDER"], filename)
     fileob.save(save_path)
+    session["filepath"] = save_path
     return "successful_upload"
 
+
+@app.route("/process", methods=["GET", "POST"])
 def process_image():
-    return render_template('test.html')
+    return facesearch.process_image(session["filepath"])
+
 
 if __name__ == '__main__':
     app.run(debug=True)
